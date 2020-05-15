da693adf215c32d48f9ffa88e681310a21acacc2
sato daichi <ds44.roy415ksk@icloud.com>
2020-05-15 16:22:47 +0900

    追加

---
+++ Bar.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Bar : MonoBehaviour
+{
+    //最大
+    int maxHp = 100;
+    //今
+    int Hp; 
+
+    public Slider slider;
+
+    void Start()
+    {
+        //フル
+        slider.value = 1;
+
+        Hp = maxHp;
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if(collider.gameObject.tag == "Enemy")
+        {
+            int damage = 10;
+            // Debug.Log("damage : " + damage);
+
+            Hp = Hp - damage;
+
+            slider.value = (float)Hp / (float)maxHp;
+           
+        }
+    }
+}
---
+++ RotationWall.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class RotateWall : Trap
+{
+    float rotateX;
+    float rotateY;
+    float rotateZ;
+
+    void Update()
+    {
+        transform.Rotate(new Vector3(rotateX, rotateY, rotateZ) * Time.deltaTime, Space.World);
+    }
+}

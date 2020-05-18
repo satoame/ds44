22b796c608c0220344a9de5334ec1e1859da3f28
sato daichi <ds44.roy415ksk@icloud.com>
2020-05-18 13:25:25 +0900

    追加

---
+++ Fuel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fuel : MonoBehaviour
+{
+    //経験値
+    public int f_exp;
+    public float brake;
+
+    //方向
+    private Vector3 direction;
+    private float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //散らばる速さ
+        var velocity = direction * speed;
+
+        transform.localPosition += velocity;
+
+        speed *= brake;
+
+        transform.localPosition = Scroll.ClampPosition(transform.localPosition);
+    }
+}

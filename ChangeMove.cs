e6beb04a371dd9796d94745adbc4cddd41af97a0
sato daichi <ds44.roy415ksk@icloud.com>
2020-06-01 15:39:42 +0900

    追加

---
+++ ChangeMove.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeMove : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Vector3 pos;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        pos = transform.position;
+
+        if (pos.y < 0)
+        {
+            // 速度を０。
+            rb.velocity = Vector3.zero;
+
+            //力を加える
+            //
+        }
+    }
+}
---
+++ StageName.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StageName : MonoBehaviour
+{
+    //テキスト
+    private Text stageNameText;
+
+    void Start()
+    {
+        stageNameText = this.gameObject.GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        stageNameText.color = Color.Lerp(stageNameText.color, new Color(1, 1, 1, 0), 0.8f * Time.deltaTime);
+    }
+}

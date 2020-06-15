cd9d857f8a343cb66062f18fddd9d2bfdedeffdc
sato daichi <ds44.roy415ksk@icloud.com>
2020-06-15 15:45:36 +0900

    追加

---
+++ Botshot.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Botshot : MonoBehaviour
+{
+    public GameObject eshotPrefab;
+    public float shotSpeed;
+    private int timeCount = 0;
+
+    void Update()
+    {
+        timeCount += 1;
+
+        //タイム
+        if (timeCount % 5 == 0)
+        {
+            GameObject enemyShot = Instantiate(eshotPrefab, transform.position, Quaternion.identity) as GameObject;
+            Rigidbody2D enemyShotRb = enemyShot.GetComponent<Rigidbody2D>();
+            enemyShotRb.AddForce(transform.forward * shotSpeed);
+            Destroy(enemyShot, 20.0f);
+        }
+        
+        if (timeCount == 500)
+        {
+            this.gameObject.AddComponent<Rota>().pos = new Vector3(0, -90, 0);
+        }
+    }
+}
---
+++ Rota.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rota : MonoBehaviour
+{
+    public Vector2 pos;
+
+    void Update()
+    {
+        transform.Rotate(new Vector3(pos.x, pos.y) * Time.deltaTime);
+    }
+}

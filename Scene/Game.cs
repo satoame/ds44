933f347a163577df91f5e076526b32155268eb13
sato_481 <49856332+satoame@users.noreply.github.com>
2020-08-04 14:29:27 +0900

    Add files via upload

---
+++ Scene/Game.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Game : MonoBehaviour
+{
+    public void OnRetry()
+    {
+        SceneManager.LoadScene("Game");
+    }
+}
